986d3695bbbc363ac2ca63325ab1d45f539cd9db
Kuzma <kuzmakulisin@macbook-pro-kuzma.local>
2019-01-15 17:11:07 +0300

    Initial Commit

---
+++ HW 3.9 Simple Pizza/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HW 3.9 Simple Pizza
+//
+//  Created by Kuzma on 15/01/2019.
+//  Copyright © 2019 Kuzma Kulishin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
